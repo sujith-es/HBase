849c79f713bad1bc1e646d47644a2aab59088f2b
cloudera <cloudera@quickstart.cloudera>
2016-10-06 13:31:53 +0530

    included Put and delete methods.

--- src/main/java/com/learn/hbase/GettingStarted.java
+++ src/main/java/com/learn/hbase/GettingStarted.java
@@ -7,6 +7,8 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
@@ -27,25 +29,35 @@ public class GettingStarted {
 		ResultScanner resultScan = table.getScanner(scan);
 
 		try {
+			//
+			// for (Result res : resultScan) {
+			// System.out.println(Bytes.toString(res.getRow()));
+			// System.out.println(Bytes.toString(res.getValue(
+			// "cf1".getBytes(), "column1".getBytes())));
+			// System.out.println(Bytes.toString(res.getValue(
+			// "cf1".getBytes(), "column2".getBytes())));
+			//
+			// }
 
-			for (Result res : resultScan) {
-				System.out.println(Bytes.toString(res.getRow()));
-				System.out.println(Bytes.toString(res.getValue(
-						"cf1".getBytes(), "column1".getBytes())));
-				System.out.println(Bytes.toString(res.getValue(
-						"cf1".getBytes(), "column2".getBytes())));
+			// Put put = new Put("3".getBytes());
+			//
+			// put.addColumn("cf1".getBytes(), "column1".getBytes(),
+			// "value1".getBytes());
+			//
+			// put.addColumn("cf1".getBytes(), "column2".getBytes(),
+			// "value2".getBytes());
 
-			}
+			// table.put(put);
 
-			Put put = new Put("3".getBytes());
+			Get get = new Get("3".getBytes());
+			Result result = table.get(get);
+			System.out.println(Bytes.toString(result.getValue("cf1".getBytes(),
+					"column1".getBytes())));
+			System.out.println(Bytes.toString(result.getValue("cf1".getBytes(),
+					"column2".getBytes())));
 
-			put.addColumn("cf1".getBytes(), "column1".getBytes(),
-					"value1".getBytes());
-
-			put.addColumn("cf1".getBytes(), "column2".getBytes(),
-					"value2".getBytes());
-
-			table.put(put);
+			// Delete delete = new Delete("3".getBytes());
+			// table.delete(delete);
 
 		} catch (Exception e) {
 			System.out.println(e.toString());
